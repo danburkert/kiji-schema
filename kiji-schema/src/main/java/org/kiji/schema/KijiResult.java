6364a823fe66ab08a3dc0534beafa61ed86d79f0
Dan Burkert <dan@danburkert.com>
2014-06-29 18:09:55 -0700

    Refactored the KijiResult API

--- kiji-schema/src/main/java/org/kiji/schema/KijiResult.java
+++ kiji-schema/src/main/java/org/kiji/schema/KijiResult.java
@@ -18,15 +18,58 @@
  */
 package org.kiji.schema;
 
+import java.util.Iterator;
+
+import com.google.common.base.Function;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.LinkedHashMultiset;
+
 import org.kiji.annotations.ApiAudience;
 import org.kiji.annotations.ApiStability;
 import org.kiji.annotations.Inheritance;
 
-/** Wraps underlying database results to provide iteration and decoding. */
+/**
+ * A view of a row in a Kiji table.
+ *
+ * <p>
+ *   The view allows access to the columns specified in a {@code KijiDataRequest}. The primary
+ *   method of accessing the {@link KijiCell}s in the row is through the {@link #iterator()} method.
+ *   The {@link #getColumnResult} method can be used to narrow the view of the row to a specific
+ *   column or column family.
+ * </p>
+ *
+ * <h2>Type Safety</h2>
+ *
+ * <p>
+ *   The {@code KijiResult} API is not compile-time type safe if {@link #getColumnResult} is used
+ *   inappropriately. {@code getColumnResult} allows the caller to specify the value-type of
+ *   {@code KijiCell}s in the requested column. If the wrong type is supplied, a runtime
+ *   {@link java.lang.ClassCastException} will be thrown.
+ * </p>
+ *
+ * <p>
+ *   In particular, users should be cautious of specifying a type-bound (other than {@link Object})
+ *   on group-type families. It is only appropriate to specify a more specific type bound over a
+ *   group-type family when the family contains columns of a single type, or the
+ *   {@code KijiDataRequest} only contains columns from the family of a single type.
+ * </p>
+ *
+ * <h2>Thread Safety</h2>
+ *
+ * <p>
+ *   {@code KijiResult} implementations may not be relied upon to be thread safe, and thus should
+ *   not be shared between threads.
+ * </p>
+ *
+ * @param <T> the type of {@code KijiCell} values in the view. The result of scans and gets have a
+ *    type of {@code Object} to indicate that a specific bound over arbitrary columns can not be
+ *    inferred. Users may request a more specific type when calling {@link #getColumnResult},
+ *    but be aware of the type-safety warnings above.
+ */
 @ApiAudience.Framework
 @ApiStability.Experimental
 @Inheritance.Sealed
-public interface KijiResult extends Iterable<KijiCell<Object>> {
+public interface KijiResult<T> extends Iterable<KijiCell<T>> {
 
   /**
    * Get the EntityId of this KijiResult.
@@ -43,87 +86,179 @@ public interface KijiResult extends Iterable<KijiCell<Object>> {
   KijiDataRequest getDataRequest();
 
   /**
-   * Get the most recent cell from the given column.
-   *
-   * <p>
-   *   The column must be fully qualified and may not be marked as paged in the data request which
-   *   defines this KijiResult.
-   * </p>
-   *
-   * @param column name of the Kiji column from which to get the most recent cell.
-   * @param <T> type of the value in the given column.
-   * @return the most recent cell from the given column or null if no cells exist.
-   * @throws NullPointerException if the given column is not included in the data request which
-   *     defines this KijiResult.
-   * @throws java.lang.IllegalArgumentException if the given column is marked as paged in the data
-   *     request which defines this KijiResult.
-   */
-  <T> KijiCell<T> getMostRecentCell(KijiColumnName column);
-
-  /**
-   * Get the version of the cell in the given column at the given timestamp.
-   *
-   * <p>
-   *   The column must be fully qualified and may not be marked as paged in the data request which
-   *   defines this KijiResult.
-   * </p>
-   *
-   * @param column name of the Kiji column from which to get the cell at the given timestamp.
-   * @param timestamp timestamp at which to get the cell in the given column.
-   * @param <T> type of the value in the given column.
-   * @return the version of the cell in the given column at the given timestamp or null if none
-   *     exists.
-   * @throws NullPointerException if the given column is not included in the data request which
-   *     defines this KijiResult.
-   * @throws java.lang.IllegalArgumentException if the given column is marked as paged in the data
-   *     request which defines this KijiResult.
-   */
-  <T> KijiCell<T> getCell(KijiColumnName column, long timestamp);
-
-  /**
-   * Get an iterator of the cells in the given qualified column or map-type family.
-   *
-   * <p>
-   *   The column may not be a group-type family. The column may be marked as paged or unpaged in
-   *   the data request which defines this KijiResult.
-   * </p>
-   *
-   * <p>
-   *   Multiple calls to <code>iterator(column)</code> with the same column will return separate
-   *   iterators. If the column is paged, data will be re-requested from the underlying store and
-   *   two active iterators on a paged column will hold two pages in memory simultaneously.
-   * </p>
-   *
-   * <p>
-   *   If the given column is a map-type family qualifiers are sorted lexicographically. Within one
-   *   qualified column, cells are sorted most recent first.
-   * </p>
-   *
-   * @param column qualified column or map-type family from which to get an iterator of cells.
-   * @param <T> type of the value in the given column.
-   * @return an iterator of the cells in the given qualified column or map-type family.
-   * @throws NullPointerException if the given column is not included in the data request which
-   *     defines this KijiResult.
-   */
-  <T> KijiResultIterator<T> iterator(KijiColumnName column);
-
-  /**
    * {@inheritDoc}
    *
    * <p>
-   *   Multiple calls to <code>iterator()</code> will return separate iterators. If the column data
-   *   request which defines this KijiResult contains paged columns, those pages will be
-   *   re-requested from the underlying store. Two active iterators may hold two pages of data in
-   *   memory simultaneously.
+   *   Multiple calls to {@code iterator()} will return independent iterators. If the
+   *   {@code DataRequest} which defines this {@code KijiResult} contains paged columns, those pages
+   *   will be requested from the underlying store for each call to this method. Two active
+   *   iterators may hold two pages of data in memory simultaneously.
+   * </p>
+   *
+   * <h2>Ordering</h2>
+   *
+   * <p>
+   *   {@code KijiResult} provides four guarantees about the ordering of the {@code KijiCell}s
+   *   in the returned iterator:
+   * </p>
+   *
+   * <ol>
+   *   <li> Non-paged columns appear before paged columns.
+   *   <li> Within a column family, qualified columns will appear in ascending qualifier order.
+   *   <li> Within a qualified-column, {@code KijiCell}s will appear in descending version order.
+   *   <li> Relative ordering of columns will be consistent in repeated calls to
+   *        {@link #iterator()} for the same {@code KijiResult}.
+   * </ol>
+   *
+   * <p>
+   *   Where two of these guarantees apply, the guarantee higher in the list takes precedence. For
+   *   instance, if a {@code KijiDataRequest} contains requests for a paged column "fam:qual1",
+   *   and a non-paged column "fam:qual2", then "fam:qual2" will be returned first in the iterator,
+   *   despite {@code "qual1"} sorting before {@code "qual2"}.
    * </p>
    *
    * <p>
-   *   The order of columns in the iterator is not defined, but all non-paged columns will appear
-   *   before all paged columns. Within one qualified column, cells are sorted most recent first.
+   *   No guarantee is made about the ordering among families.
    * </p>
    *
    * @return an iterator over all the cells in this KijiResult.
+   * @throws KijiIOException on unrecoverable I/O exception.
    */
   @Override
-  KijiResultIterator<Object> iterator();
+  Iterator<KijiCell<T>> iterator();
+
+  /**
+   * Get a view of this {@code KijiResult} restricted to the provided column.
+   *
+   * <p>
+   *   The column may be a column family, or a fully-qualified column. If this {@code KijiResult}
+   *   does not include the provided column, then the resulting {@code KijiResult} will be empty,
+   *   and its {@code KijiDataRequest} will not contain any columns. If the provided column is
+   *   non-paged, then the cells will be shared with the returned {@code KijiResult}.
+   * </p>
+   *
+   * <p>
+   *   This method allows the caller to specify a type-bound on the values of the provided column.
+   *   The caller should be careful to only specify an appropriate type. If the type is too specific
+   *   (or wrong), runtime {@link java.lang.ClassCastException} will be thrown when the returned
+   *   {@code KijiResult} is used. See the Type Safety section of {@code KijiResult}'s documentation
+   *   for more details.
+   * </p>
+   *
+   * @param column which the view of this {@code KijiResult} will be restricted.
+   * @param <T> type of the value in the given column.
+   * @return a {@code KijiResult} which contains only the provided column.
+   */
+  <T> KijiResult<T> getColumnResult(KijiColumnName column);
+
+  /**
+   * Provides helper methods for working with {@link KijiResult}s.
+   */
+  public static class Helpers {
+
+    /**
+     * Returns a {@link com.google.common.collect.Multiset} of {@code KijiColumnName} to version
+     * count for the columns in the provided {@code KijiResult}.
+     *
+     * <p>
+     *   The returned {@code Multiset}'s iteration order is the order of columns returned by this
+     *   {@code KijiResult}, and the count is the number of versions of the column. Note that this
+     *   will require fetching paged columns.
+     * </p>
+     *
+     * <p>
+     *   If the provided {@code KijiResult} will only be used retrieving the columns of the row, the
+     *   {@link org.kiji.schema.filter.StripValueRowFilter} or
+     *   {@link org.kiji.schema.filter.StripValueColumnFilter} can be used to avoid retrieving the
+     *   values of each {@code KijiCell}.
+     * </p>
+     *
+     * <p>
+     *   If the provided {@code KijiResult} will only be used retrieving the columns of the row, and
+     *   the version count is not important, set the max number of versions to retrieve in the
+     *   {@link KijiDataRequest} for each column to {@code 1} to avoid retrieving unnecessary
+     *   {@code KeyValues}s.
+     * </p>
+     *
+     * @param result {@code KijiResult} for which to retrieve columns and version counts.
+     * @return a {@code Multiset} of {@code KijiColumnName} to version count.
+     */
+    LinkedHashMultiset<KijiColumnName> getColumns(KijiResult<?> result) {
+      final LinkedHashMultiset<KijiColumnName> set = LinkedHashMultiset.create();
+      for (KijiCell<?> cell : result) {
+        set.add(cell.getColumn());
+      }
+      return set;
+    }
+
+    /**
+     * Return the first {@code KijiCell} in the first column of this {@code KijiResult}.
+     *
+     * <p>
+     *   Note that if all columns in the {@code KijiResult} are paged, then retrieving the first
+     *   cell will require fetching a full page.
+     * </p>
+     *
+     * @param <T> type of {@code KijiCell}s in the provided {@code KijiResult}.
+     * @return the first {@code KijiCell} in the result, or {@code null} if the result is empty.
+     */
+    <T> KijiCell<T> getFirst(KijiResult<T> result) {
+      return Iterables.getFirst(result, null);
+    }
+
+    /**
+     * Return the value of the first {@code KijiCell} in the first column of this
+     * {@code KijiResult}.
+     *
+     * <p>
+     *   Note that if all columns in the {@code KijiResult} are paged, then retrieving the first
+     *   cell will require fetching a full page.
+     * </p>
+     *
+     * <p>
+     *   It is not possible to distinguish between an empty result and a {@code KijiCell} which
+     *   contains a null value using this method. If the difference is material, use
+     *   {@link #getFirst} and explicitly check whether the resulting {@code KijiCell} is
+     *   {@code null}.
+     * </p>
+     *
+     * @param <T> type of {@code KijiCell}s in the provided {@code KijiResult}.
+     * @return the value of the first {@code KijiCell} in the result, or {@code null} if the result
+     *    is empty, or the value is {@code null}.
+     */
+    <T> T getFirstValue(KijiResult<T> result) {
+      final KijiCell<T> first = getFirst(result);
+      if (first == null) {
+        return null;
+      } else {
+        return first.getData();
+      }
+    }
+
+    /**
+     * Return an {@link Iterable} of the {@code KijiCell} values in this {@code KijiResult}. Note
+     * that the returned {@code Iterable} will require fetching paged columns when iterated exactly
+     * as {@code KijiResult}s do.
+     *
+     * @param result the {@code KijiResult} from which to retrieve values.
+     * @param <T> type of values in the {@code KijiResult}.
+     * @return the values in the provided {@code KijiResult}.
+     */
+    <T> Iterable<T> getValues(KijiResult<T> result) {
+      return Iterables.transform(
+          result,
+          new Function<KijiCell<T>, T>() {
+            @Override
+            public T apply(final KijiCell<T> cell) {
+              return cell.getData();
+            }
+          });
+    }
+
+    /**
+     * Private constructor for utility class.
+     */
+    private Helpers() {
+    }
+  }
 }
